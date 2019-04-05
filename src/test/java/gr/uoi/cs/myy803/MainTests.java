4eb179370c147cb15647243cf5c017663741058a
gzougianos <g.zougianos@gmail.com>
2019-04-05 18:36:24 +0300

    test the name of new documents

--- src/test/java/gr/uoi/cs/myy803/MainTests.java
+++ src/test/java/gr/uoi/cs/myy803/MainTests.java
@@ -1,6 +1,7 @@
 package gr.uoi.cs.myy803;
 
 import static org.junit.Assert.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 import org.junit.Test;
@@ -11,10 +12,12 @@ import myy803.model.Document;
 import myy803.model.DocumentType;
 
 public class MainTests {
+	private static final DocumentManager DM = DocumentManager.INSTANCE;
+
 	@Test
 	public void allDocTypesHaveTemplate() {
 		for (DocumentType docType : DocumentType.values()) {
-			Document doc = DocumentManager.INSTANCE.createDocument(docType);
+			Document doc = DM.createDocument(docType);
 			assertNotEquals(doc.getContent(), "");
 		}
 	}
@@ -24,5 +27,38 @@ public class MainTests {
 		for (Command c : Command.values()) {
 			assertNotNull(c.getContent());
 		}
+	}
+
+	@Test
+	public void newDocumentName() {
+		Document doc1 = DM.createDocument(DocumentType.ARTICLE);
+		assertEquals(doc1.getPath().getName(), "unsaved.tex");
+		DM.getDocuments().add(doc1);
+
+		Document doc2 = DM.createDocument(DocumentType.ARTICLE);
+		assertEquals(doc2.getPath().getName(), "unsaved1.tex");
+		DM.getDocuments().add(doc2);
+
+		Document doc3 = DM.createDocument(DocumentType.ARTICLE);
+		assertEquals(doc3.getPath().getName(), "unsaved2.tex");
+		DM.getDocuments().add(doc3);
+
+		DM.getDocuments().remove(doc2);
+
+		Document doc4 = DM.createDocument(DocumentType.ARTICLE);
+		assertEquals(doc4.getPath().getName(), "unsaved3.tex");
+		DM.getDocuments().add(doc4);
+
+		DM.getDocuments().clear();
+
+		Document doc5 = DM.createDocument(DocumentType.ARTICLE);
+		assertEquals(doc5.getPath().getName(), "unsaved.tex");
+		DM.getDocuments().add(doc5);
+
+		for (int i = 0; i < 1000; i++) {
+			DM.getDocuments().add(DM.createDocument(DocumentType.ARTICLE));
+		}
+
+		assertEquals(DM.getDocuments().get(DM.getDocuments().size() - 1).getName(), "unsaved1000");
 	}
 }
